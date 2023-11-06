ce31fd1f75f61dff85bce33b10f48b680b08f8fa
jagritiyadav <135732838+jagritiyadav@users.noreply.github.com>
2023-11-06 10:58:40 +0545

    Programs

---
+++ Jagriti_Yadav.py
@@ -0,0 +1,25 @@
+#QN no 1
+print("Hello World!")
+
+#QN no 2
+print("Hello Jagriti")
+
+#Qn no 3
+C=38.4
+F=C*(9/5)+32
+print(C)
+print(F)
+
+#Qn no 4
+total_runs=48426
+Batted=1014
+NotOut=162
+innings=Batted - NotOut
+average_batting=total_runs/innings
+print(average_batting)
+
+#Qn no 5
+group=24
+FirstGroup=113//group
+First1Group=113%group
+print("Number of group: ",FirstGroup,"Remainder students: ",First1Group)
